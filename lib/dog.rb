118d8ca6363115070a0a83c2c7e194787c2b995b
Keith Isham <kisham8899@gmail.com>
2019-04-13 13:34:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,10 @@
 class Dog
   attr_accessor :name, :breed
 
-def initialize(name, breed="Mutt")
+def initialize(name)
   @name = name
-  @breed = breed
+
 end
-  def breed(breed = "Mutt")
-    "#{@breed}"
-  end
+
 
 end
