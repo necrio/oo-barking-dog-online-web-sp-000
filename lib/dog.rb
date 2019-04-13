0747ec29a76bdabc5a26ef9581b65449a51cc3e6
Keith Isham <kisham8899@gmail.com>
2019-04-13 13:37:10 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Dog
+  attr_accessor :name, :breed
+
+# def initialize(name)
+#   @name = name
+#
+# end
+
+def bark
+  puts "woof!"
+end
+
+end
+
+max = Dog.new
+max.name = "Max"
